ddde36f18772e3c422029dd1eea12dff626ec492
mohammad molavi <mohamadmolavi98@gmail.com>
2024-03-10 11:56:57 +0330

    develop contact component

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,24 +1,39 @@
 import { useState } from "react";
+import ContactsList from "./ContactsList";
 
 function Contacts() {
-    const [contact,setContact]=useState({
-        name:"",
-        lastName:"",
-        email:"",
-        phone:"",
-    })
-    const changeHandler=(event)=>{
-        
-    }
+  const [contacts, setContacts] = useState([]);
+  const [contact, setContact] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+  const changeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setContact((contact) => ({ ...contact, [name]: value }));
+  };
+  const addHandler = () => {
+    setContacts((contacts) => [...contacts, contact]);
+    setContact({
+      name: "",
+      lastName: "",
+      email: "",
+      phone: "",
+    });
+    console.log(contacts);
+  };
   return (
     <div>
       <div>
-        <input type="text" placeholder="Name" value={contact.name} onChange={changeHandler} />
-        <input type="text" placeholder="Last Name" value={contact.lastName} onChange={changeHandler} />
-        <input type="email" placeholder="Email" value={contact.email} onChange={changeHandler} />
-        <input type="number" placeholder="Phone" value={contact.phone} onChange={changeHandler} />
-        <button>Add Contact</button>
+        <input type="text" placeholder="Name" name="name" value={contact.name} onChange={changeHandler} />
+        <input type="text" placeholder="Last Name" name="lastName" value={contact.lastName} onChange={changeHandler} />
+        <input type="email" placeholder="Email" name="email" value={contact.email} onChange={changeHandler} />
+        <input type="number" placeholder="Phone" name="phone" value={contact.phone} onChange={changeHandler} />
+        <button onClick={addHandler}>Add Contact</button>
       </div>
+      <ContactsList contacts={contacts} />
     </div>
   );
 }
---
+++ src/components/ContactsList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ContactsList(props) {
+  console.log(props)
+  return (
+    <div>
+      <h1>ContactList</h1>
+    </div>
+  )
+}
+
+export default ContactsList
