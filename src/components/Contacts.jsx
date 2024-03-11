609a2aafaec086a9b4389843c57bc2aa1467f67f
mohammad molavi <mohamadmolavi98@gmail.com>
2024-03-11 15:44:52 +0330

    completed style for contacts component

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -3,6 +3,7 @@ import { v4 } from "uuid";
 
 import ContactsList from "./ContactsList";
 import inputs from "../constans/inputs.js";
+import styles from "./Contacts.module.css"
 function Contacts() {
   const [contacts, setContacts] = useState([]);
   const [alert, setAlert] = useState("");
@@ -39,15 +40,15 @@ function Contacts() {
     setContacts(newContacts);
   };
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.form}>
         {inputs.map((input, index) => (
           <input type={input.type} key={index} name={input.name} placeholder={input.placeholder} value={contact[input.name]} onChange={changeHandler} />
         ))}
         <button onClick={addHandler}>Add Contact</button>
       </div>
 
-      <div>{alert && <p>{alert}</p>}</div>
+      <div className={styles.alert}>{alert && <p>{alert}</p>}</div>
       <ContactsList contacts={contacts}  deleteHandler={deleteHandler} />
     </div>
   );
